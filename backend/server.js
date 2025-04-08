79a5106cf60950fe9ca1ed2f26edf48120bf5ada
Manohar Kumar <icsixyz@gmail.com>
2025-04-08 19:06:48 +0530

    Backend files Intialized

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv";
+const app = express();
+const PORT = process.env.PORT || 8000 ; 
+dotenv.config();
+
+app.get("/" , (req , res ) => {
+    res.status(200).send("Server is running");
+});
+
+app.listen(PORT , () => {
+    console.log(`Server is running PORT : ${PORT}`);
+});
